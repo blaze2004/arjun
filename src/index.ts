97bb504cdcab34b82dd31804b490c11549f533bb
Piyush Duggal <duggalpiyush0@gmail.com>
2023-02-04 09:53:44 +0530

    fixed structure

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import { Client, Message } from 'whatsapp-web.js';
-import QRCode from "qrcode-terminal";
+import * as WAWebJS from "whatsapp-web.js";
+import qrcode from 'qrcode-terminal';
 
-const client: Client=new Client({});
+const client = new WAWebJS.Client({});
 
 client.on('qr', (qr: string) => {
-    QRCode.generate(qr, { small: true });
-    console.log('QR code recieved.', qr);
+  qrcode.generate(qr, { small: true });
+  console.log('QR code recieved.', qr);
 });
 
 client.on('ready', () => {
-    console.log("Client is ready.");
+  console.log('Client is ready.');
 });
 
-client.on('disconnected', (reason) => {
-    client.destroy();
-    console.log('Client was logged out', reason);
+client.on('disconnected', (reason: WAWebJS.WAState) => {
+  client.destroy();
+  console.log('Client was logged out', reason);
 });
 
-client.on('message', (msg: Message) => {
-    if (msg.body==='!ping') {
-        msg.reply('pong');
-    } else {
-        console.log(msg.body);
-    }
+client.on('message', (msg: WAWebJS.Message) => {
+  if (msg.body === '!ping') {
+    msg.reply('pong');
+  } else {
+    console.log(msg.body);
+  }
 });
 
 client.initialize();
--- src/processMessage.ts
+++ src/processMessage.ts
@@ -1,14 +1,13 @@
-import { Message } from "whatsapp-web.js";
+import { Message } from 'whatsapp-web.js';
 
-export default function ProcessMessage(msg: Message) {
-
-    // Implement Watson connection here
-    // Remove below code, it's just a sample
-
-    if (msg.body==="!ping") {
-        msg.reply("pong");
-    } else {
-        msg.reply("Hey, I am taskmaster a whatsapp bot.\n I can't understand humans random language, so please try to be precise.");
-    }
-
-}
+export const processMessage = (msg: Message) => {
+  // Implement Watson connection here
+  // Remove below code, it's just a sample
+  if (msg.body === '!ping') {
+    msg.reply('pong');
+  } else {
+    msg.reply(
+      "Hey, I am taskmaster, a WhatsApp Bot.\n I can't understand human language, so please try to be precise."
+    );
+  }
+};
