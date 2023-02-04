52c1713228f17ce53d154bc8c6ed0c368e4cf18f
Piyush Duggal <duggalpiyush0@gmail.com>
2023-02-04 09:55:45 +0530

    init client

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import * as WAWebJS from "whatsapp-web.js";
+import * as WAWebJS from 'whatsapp-web.js';
+import { Client, LocalAuth } from 'whatsapp-web.js';
 import qrcode from 'qrcode-terminal';
 
-const client = new WAWebJS.Client({});
+const client = new Client({
+  puppeteer: {
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },
+  authStrategy: new LocalAuth({
+    dataPath: `${__dirname}/session`,
+  }),
+});
 
 client.on('qr', (qr: string) => {
   qrcode.generate(qr, { small: true });
