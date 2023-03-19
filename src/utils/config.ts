749c71cf1c8b9daf5eacd02f0c8fa5d5f4a533e4
Piyush Duggal <duggalpiyush0@gmail.com>
2023-03-19 15:26:24 +0530

    modifed env var

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,20 +1,20 @@
 import { config } from 'dotenv';
 
-if (process.env.NODE_ENV!=='production') {
-    config();
+if (process.env.NODE_ENV !== 'production') {
+  config();
 }
 
-const environmentVariables={
-    port: process.env.PORT||3000,
-    nodeEnv: process.env.NODE_ENV,
-    sessionSecret: process.env.SESSION_SECRET||'temperory_development_secret',
-    databaseUrl: process.env.DATABASE_URL,
-    openAIApiKey: process.env.OPENAI_API_KEY,
-    googleOauthClientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
-    googleOauthClientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-    googleOauthRedirectUri: process.env.SUPABASE_URL+'/auth/v1/callback',
-    supabaseUrl: process.env.SUPABASE_URL as string,
-    supabaseKey: process.env.SUPABASE_ANON_KEY as string,
-}
+const environmentVariables = {
+  port: process.env.PORT || 3000,
+  nodeEnv: process.env.NODE_ENV,
+  sessionSecret: process.env.SESSION_SECRET || 'temperory_development_secret',
+  databaseUrl: process.env.DATABASE_URL,
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  googleOauthClientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+  googleOauthClientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+  googleOauthRedirectUri: process.env.SUPABASE_URL + '/auth/v1/callback',
+  supabaseUrl: process.env.SUPABASE_URL as string,
+  supabaseKey: process.env.SUPABASE_SERVICE_KEY as string,
+};
 
 export default environmentVariables;
