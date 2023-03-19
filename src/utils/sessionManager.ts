39daf3f0ae39c8128e370189e2d0865902c85da4
Piyush Duggal <duggalpiyush0@gmail.com>
2023-03-19 15:27:39 +0530

    refactor

--- src/utils/sessionManager.ts
+++ src/utils/sessionManager.ts
@@ -1,45 +1,38 @@
-import { Request } from "express";
-import session, { SessionOptions } from "express-session";
-import pgSession from "connect-pg-simple";
-import { Pool } from "pg";
-import { randomUUID } from "crypto";
-import environmentVariables from "./config";
+import { Request } from 'express';
+import session, { SessionOptions } from 'express-session';
+import pgSession from 'connect-pg-simple';
+import { Pool } from 'pg';
+import { randomUUID } from 'crypto';
+import environmentVariables from './config';
 
 export const pgPool = new Pool({
-    connectionString: environmentVariables.databaseUrl,
+  connectionString: environmentVariables.databaseUrl,
 });
 
 const pgSessionStore = pgSession(session);
 export const sessionStore = new pgSessionStore({
-    pool: pgPool,
-    tableName: "user_sessions",
+  pool: pgPool,
+  tableName: 'user_sessions',
 });
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 const genId = (req: Request) => {
-
-    if (req && req.body) {
-        if (
-            req.body.owner &&
-            req.body.messageObj
-        ) {
-            const customerPhoneNumber: string = req.body.messageObj.phone;
-            return customerPhoneNumber;
-        }
-    }
-
+  if (req.body.phone) {
+    return req.body.messageObj.phone;
+  } else {
     return randomUUID();
-}
-
-export const sessionConfig: SessionOptions = {
-    name: "SESS_ID",
-    secret: environmentVariables.sessionSecret,
-    resave: false,
-    genid: genId,
-    saveUninitialized: false,
-    store: sessionStore,
-    cookie: {
-        maxAge: 100*365*24*60*60*1000,
-        secure: environmentVariables.nodeEnv === "production",
-    }
+  }
 };
 
+export const sessionConfig: SessionOptions = {
+  name: 'SESS_ID',
+  secret: environmentVariables.sessionSecret,
+  resave: false,
+  genid: genId,
+  saveUninitialized: false,
+  store: sessionStore,
+  cookie: {
+    maxAge: 100 * 365 * 24 * 60 * 60 * 1000,
+    secure: environmentVariables.nodeEnv === 'production',
+  },
+};
--- src/validator/validateResources.ts
+++ src/validator/validateResources.ts
@@ -1,15 +1,14 @@
-import { NextFunction, Request, Response } from "express";
-import { AnyObject } from "yup";
+import { NextFunction, Request, Response } from 'express';
+import { AnyObject } from 'yup';
 
 export const validate =
-  (schema: AnyObject) =>
-  (req: Request, res: Response, next: NextFunction) => {
+  (schema: AnyObject) => (req: Request, res: Response, next: NextFunction) => {
     schema
       .validate(req.body)
       .then(() => {
         next();
       })
-      .catch((err:any) => {
+      .catch((err: any) => {
         return res.status(400).json({ error: err.message });
       });
   };
