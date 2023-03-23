150e23656cff4a7485c93e596b147a56273a375d
Shubham Tiwari <shubhamtiwari06112004@gmail.com>
2023-03-23 16:26:46 +0000

    fixed event timezone issue

--- src/process/onboarding.ts
+++ src/process/onboarding.ts
@@ -1,30 +1,23 @@
 import { Request } from "express";
 import Process from ".";
 import replies from "../constants/replies";
-import { toCapitalCase } from "../utils/messageBuilder";
 
 class UserOnboarding extends Process {
   constructor(pid: string) {
     super(pid);
     this.questions = [
       {
-        question: "*What is your current profession or field of study?*",
+        question: "*Hey, How are you doing?*",
         validateAnswer: (answer: string) => answer.length > 0,
-        errorMsg: "Please enter a valid profession.",
+        errorMsg: "Please enter a valid answer.",
         answer: null
-      },
-      {
-        question: "*Can you tell me a little more about yourself (anything you would like to tell)?*",
-        validateAnswer: (answer: string) => answer.length > 0,
-        errorMsg: "Please enter a valid profession.",
-        answer: null
-      },
+      }
     ];
   }
 
   async postProcessAction(req: Request) {
     await super.postProcessAction(req);
-    return [`🍻 Welcome ${toCapitalCase(req.user.name || "There")}!`, replies.welcomeMessage as string];
+    return [replies.welcomeMessage as string];
   }
 }
 
